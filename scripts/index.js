dc0c58fd2e2348f25768016d66fcb73364e0e3bc
nobodayhh <irishuang998@yahoo.com>
2024-08-02 11:36:39 -0400

    js1
    hhh

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const initialCard0 ={
+    name: "Yosemite Valley",
+    link: new URL("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"),
+};
+
+const initialCard1 ={
+    name: "Lake Louise",
+    link: new URL("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"),
+};
+
+const initialCard2 ={
+    name: "Bald Mountains",
+    link: new URL("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"),
+};
+
+const initialCard3 ={
+    name: "Latemar",
+    link: new URL("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"),
+};
+
+const initialCard4 ={
+    name: "Vanoise National Park",
+    link: new URL("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"),
+};
+
+const initialCard5 ={
+    name: "Lago di Braies",
+    link: new URL("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"),
+};
+
+
+const initialCards = [initialCard0, initialCard1, initialCard2, initialCard3, initialCard4, initialCard5];
